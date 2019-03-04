e5d25ce55c386e34ca2ec7e278c4a2fa4fa69c17
ingleonelrv <ing.leonelrv@gmail.com>
2019-03-03 20:25:05 -0600

    client side chat: working

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+import Chat from './src/Chat'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Chat />
+    );
+  }
+}
